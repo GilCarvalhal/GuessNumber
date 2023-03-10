6e2051260385ffe8eb7b4431e9a5c5f425fd531c
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-03-09 22:18:14 -0300

    Iniciando o Javascript do projeto

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+
+const statusTitle = document.getElementById('status');
+const attempt = document.getElementById('attempt');
+const input = document.getElementById('kick');
+const result = document.getElementById('result');
+const btnRestart = document.getElementById('btn-Restart');
+
+function handleSubmit(e) {
+  alert('Olá');
+}

8b11d23a0501c1dfa49e3702bc689357cbd539b4
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-03-10 20:51:08 -0300

    Lógica principal da aplicação

--- js/index.js
+++ js/index.js
@@ -1,12 +1,29 @@
-const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit);
+const form = document.getElementById("form");
+form.addEventListener("submit", handleSubmit);
 
-const statusTitle = document.getElementById('status');
-const attempt = document.getElementById('attempt');
-const input = document.getElementById('kick');
-const result = document.getElementById('result');
-const btnRestart = document.getElementById('btn-Restart');
+const statusTitle = document.getElementById("status");
+const attempt = document.getElementById("attempt");
+const inputValue = document.getElementById("kick");
+const result = document.getElementById("result");
+const btnRestart = document.getElementById("btn-Restart");
+
+const numberDraw = Math.round(Math.random() * 10);
 
 function handleSubmit(e) {
-  alert('Olá');
+  e.preventDefault();
+
+  const kick = inputValue.value;
+
+  if (!kick) {
+    alert("Digite algum valor!");
+    return;
+  }
+
+  if (numberDraw == kick) {
+    alert("ACERTOU!");
+  } else if (numberDraw > kick) {
+    alert("O número é maior!");
+  } else if (numberDraw < kick) {
+    alert("O número é menor!");
+  }
 }
